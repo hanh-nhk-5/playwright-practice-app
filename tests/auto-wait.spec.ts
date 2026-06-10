350ed0cdca2c55801122cda6c3b19d6428a9b504
-hanhnhk <hanhnhk@gmail.com>
2026-06-10 09:30:07 -0500

    correct an incorrect comment in auto-wait.spec.ts

--- tests/auto-wait.spec.ts
+++ tests/auto-wait.spec.ts
@@ -5,14 +5,13 @@ test('auto wait', async({page})=>{
     await page.getByRole('button', {name:'Button Triggering AJAX Request'}).click();
         
     // await page.waitForSelector('.bg-success');// Wait for selector
-    // await page.waitForResponse('http://uitestingplayground.com/ajaxdata');//Wait for url response - NOT Recommend
-
+    // await page.waitForResponse('http://uitestingplayground.com/ajaxdata');
+    // await page.waitForLoadState('networkidle') //NOT Recommend
     
     const responseLabel = page.locator('.bg-success');
     // await responseLabel.waitFor({state: 'visible', timeout:16000})//wait for element state
     await expect(responseLabel).toBeVisible({timeout: 16000});//wait using assertion 
     const value = await responseLabel.textContent();
-
     expect(value).toEqual('Data loaded with AJAX get request.');
     
 })
